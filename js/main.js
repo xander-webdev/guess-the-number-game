2dff339abd91cde643c202cb906e2f767194935a
xander-webdev <xwernecke@gmail.com>
2020-06-03 19:59:34 +0000

    updated read me and added bottstrap grid system to ensure it was responsive on mobile device

--- js/main.js
+++ js/main.js
@@ -62,23 +62,22 @@ const game = () => {
       return;
     }
   };
-//   const toggleHints = () => {
-//     const hints = document.querySelector("#hint");
-//     const hintOn = document.querySelector("#hint-btnOff");
-//     const hintOff = document.querySelector("#hint-btnOn");
+  const toggleHints = () => {
+    const hints = document.querySelector("#hints-div #hint");
+    const hintOn = document.querySelector("#hint-btnOff");
+    const hintOff = document.querySelector("#hint-btnOn");
 
-//     hintOn.addEventListener("click", function () {
-//         hints.classList.add("fadeIn");
-//         hints.classList.remove("fadeOut");
+    hintOn.addEventListener("click", function () {
+        hints.classList.add("fadeIn");
+        hints.classList.remove("fadeOut");
 
-//     });
-//     hintsOff.addEventListener("click", function(){
-//         hints.classList.add("fadeOut");
-//         hints.classList.remove("fadeIn")
-//     })
-//   };
-  
-
+    });
+    hintsOff.addEventListener("click", function(){
+        hints.classList.add("fadeOut");
+        hints.classList.remove("fadeIn")
+    })
+  };
+  toggleHints();
   playGame();
 
   //push hints to the DOM
